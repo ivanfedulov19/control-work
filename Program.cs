4dab62ea6a4c4e1a3011a34f10aac8f76d07d634
ivan fedulov <van4a.ya19@gmail.com>
2022-07-27 14:53:30 +0400

    Add methods Create and Fill array

--- Program.cs
+++ Program.cs
@@ -4,3 +4,12 @@ string[] CreateArray(int size)
     return new string[size];
 }
 
+string[] FillArray(string[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        System.Console.WriteLine($"Введите {i + 1} элемент массива ");
+        array1[i] = Console.ReadLine();
+    }
+    return array1;
+}
